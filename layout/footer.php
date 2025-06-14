f642b5113facde851b9057fe9398f3c9263ca6d3
Naufal Fauzan <l200230062@student.ums.ac.id>
2025-06-14 21:24:48 +0700

    Add files via upload

---
+++ layout/footer.php
@@ -0,0 +1,84 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2025 <a href="https://adminlte.io">Kelompok 3</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1
+    </div>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="assets-template/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="assets-template/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="assets-template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="assets-template/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="assets-template/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="assets-template/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="assets-template/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="assets-template/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="assets-template/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="assets-template/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="assets-template/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="assets-template/dist/js/adminlte.js"></script>
+
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="assets-template/dist/js/pages/dashboard.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="assets-template/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="assets-template/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="assets-template/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="assets-template/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="assets-template/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="assets-template/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="assets-template/plugins/jszip/jszip.min.js"></script>
+<script src="assets-template/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="assets-template/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="assets-template/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="assets-template/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="assets-template/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.3.2/js/dataTables.bootstrap5.js"></script>
+<script>
+    $(function() {
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": true,
+            "responsive": true,
+        });
+    });
+</script>
+</body>
+
+</html>
